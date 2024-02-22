3bfcd031b93095b669a41a57bca364bb914bb15f
sngovind <160743419+sngovind@users.noreply.github.com>
2024-02-22 16:09:48 +0530

    Update sample1.sql

--- sample1.sql
+++ sample1.sql
@@ -1,6 +1,7 @@
 -- available online in file 'sample1'
 DECLARE
    x NUMBER := 100;
+   v_number NUMBER := 7;
 BEGIN
    FOR i IN 1..10 LOOP
       IF MOD(i,2) = 0 THEN     -- i is even
@@ -11,4 +12,9 @@ BEGIN
       x := x + 100;
    END LOOP;
    COMMIT;
+    IF MOD(v_number, 2) = 0 THEN
+        DBMS_OUTPUT.PUT_LINE(v_number || ' is an even number.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE(v_number || ' is an odd number.');
+    END IF;
 END;
