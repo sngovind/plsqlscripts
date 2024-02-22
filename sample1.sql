0fe14e7895f07d22778ab9dc671917a3b609d9c5
sngovind <sngovind@SNGOVIND-7420.in.oracle.com>
2024-02-22 12:17:24 +0530

    updated code

--- sample1.sql
+++ sample1.sql
@@ -4,9 +4,9 @@ DECLARE
 BEGIN
    FOR i IN 1..10 LOOP
       IF MOD(i,2) = 0 THEN     -- i is even
-         INSERT INTO temp VALUES (i, x, 'i is even');
+         INSERT INTO temp VALUES (i, x, 'i is even number');
       ELSE
-         INSERT INTO temp VALUES (i, x, 'i is odd');
+         INSERT INTO temp VALUES (i, x, 'i is odd number');
       END IF;
       x := x + 100;
    END LOOP;
